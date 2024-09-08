e561eba919c516eb73acbc40cfb3102e132d454d
Wassim <512.amosimo@gmail.com>
2024-09-08 10:22:50 +0100

    brevo implementation

--- src/controllers/StepsControllers.ts
+++ src/controllers/StepsControllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import prisma from "../config/db";
 import { Site, Step, StepTypes } from "@prisma/client";
 import { replicateManagers } from "./ManagerControllers";
-
+import { sendEmail } from './../utils/sendMail';
 export const createStep = async (
   siteId: string,
   type: StepTypes,
@@ -24,8 +24,8 @@ export const createStep = async (
 };
 
 export const getCurrentStep = async (siteId: string) => {
-  if(!siteId) throw Error("siteId is required")
-  console.log(siteId)
+  if (!siteId) throw Error("siteId is required");
+  console.log(siteId);
   try {
     const step = await prisma.step.findFirst({
       where: {
@@ -36,7 +36,7 @@ export const getCurrentStep = async (siteId: string) => {
     return step;
   } catch (error) {
     console.log(error);
-    throw Error("Failed to get current step")
+    throw Error("Failed to get current step");
   }
 };
 
@@ -46,13 +46,13 @@ export const getAllSteps = async (siteId: string) => {
       where: {
         siteId,
       },
-    })
-    return steps
-  }catch(error){
-    console.log(error)
-    throw Error("Failed to get all steps")
+    });
+    return steps;
+  } catch (error) {
+    console.log(error);
+    throw Error("Failed to get all steps");
   }
-}
+};
 
 export const nextStep = async (siteId: string) => {
   const stepOrder: StepTypes[] = [
@@ -66,16 +66,19 @@ export const nextStep = async (siteId: string) => {
   ];
   try {
     const step = await getCurrentStep(siteId);
-    console.log("step: ", step)
-    if(!step) throw Error("Step unavailable")
+    console.log("step: ", step);
+    if (!step) throw Error("Step unavailable");
     const managers = await prisma.manager.findMany({
       where: {
-        stepId: step?.id
-      }
-    })
-    for(let manager of managers){
-      if(manager.validation === "PENDING" || manager.validation === "REFUSED"){
-        throw Error("Manager validation is not completed")
+        stepId: step?.id,
+      },
+    });
+    for (let manager of managers) {
+      if (
+        manager.validation === "PENDING" ||
+        manager.validation === "REFUSED"
+      ) {
+        throw Error("Manager validation is not completed");
       }
     }
     if (step) {
@@ -85,69 +88,72 @@ export const nextStep = async (siteId: string) => {
         const newStep = await createStep(siteId, nextStep);
         await prisma.step.update({
           where: {
-            id: step.id
+            id: step.id,
           },
           data: {
-            status: "COMPLETED"
-          }
-        })
+            status: "COMPLETED",
+          },
+        });
         return newStep;
       }
     }
   } catch (error) {
     console.log(error);
-    throw Error("Failed to get next step")
+    throw Error("Failed to get next step");
   }
 };
 
 export const cancelStep = async (stepId: string) => {
-
-  
   try {
     // const stepId = (await getCurrentStep(siteId))?.id
     const step = await prisma.step.update({
       where: {
-        id:stepId
+        id: stepId,
       },
       data: {
-        status: "FAILED"
-      }
-    })
-    const newStep = await createStep(step.siteId, step.type)
-    await replicateManagers(stepId, newStep.id)
+        status: "FAILED",
+      },
+    });
+    const newStep = await createStep(step.siteId, step.type);
+    await replicateManagers(stepId, newStep.id);
 
-    return newStep
-    
-  }catch(error){
-    console.log(error)
-    throw Error("Failed to cancel step")
+    return newStep;
+  } catch (error) {
+    console.log(error);
+    throw Error("Failed to cancel step");
   }
-}
+};
 
 export const updateStep = async (stepId: string, data: object) => {
   try {
     const step = await prisma.step.update({
       where: {
-        id: stepId
+        id: stepId,
       },
       data: {
-        payload: data
-      }
-    })
-  }catch(error){
-    console.log(error)
-    throw Error("Failed to update step")
+        payload: data,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw Error("Failed to update step");
   }
-}
+};
 
 export const testFeature = async (req: Request, res: Response) => {
   try {
-    
-    const {id} = req.params
-    const step = await cancelStep(id)
-    return res.status(200).json(step)
-  }catch(error){
-    console.log(error)
-    return res.status(500).json("internal server error")
+    sendEmail("hamedani.wassim@gmail.com", "Test Email", "<html><body><h1>Hello from Brevo</h1></body></html>")
+      .then((data: any) => {
+        console.log("Email sent successfully:", data);
+        res.status(200).json("weeeee")
+        
+      })
+      .catch((error: any) => {
+        console.error("Error sending email:", error);
+        res.status(400).json("failed to send email")
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("internal server error");
   }
-}
+};
