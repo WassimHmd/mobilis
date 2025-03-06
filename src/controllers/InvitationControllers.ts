f5103a24eeddd87bc822a219f81b8905275d2530
Wassim <512.amosimo@gmail.com>
2025-03-06 18:42:05 +0100

    removed @

--- src/controllers/InvitationControllers.ts
+++ src/controllers/InvitationControllers.ts
@@ -1,4 +1,4 @@
-import prisma from "@/config/db";
+import prisma from "../config/db";
 import { Request, Response } from "express";
 
 export const getInvitation = async (req: Request, res: Response) => {
